87566b72257a592baae51e6fe6a4c44ec0ea0a63
vegetKa <vegetka@gmail.com>
2018-09-15 17:25:36 +0100

    back to VSCode :)

--- coding/java/HelloCv.java
+++ coding/java/HelloCv.java
@@ -4,6 +4,9 @@ import static java.lang.System.loadLibrary;
 import static java.lang.System.out;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.core.MatOfInt;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Size;
+import org.opencv.core.Scalar;
 
 import static org.opencv.core.CvType.CV_8UC1;
 import static org.opencv.core.CvType.CV_8UC3;
@@ -16,6 +19,7 @@ public class HelloCv {
     static{
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
+
     public static void main(String[] args) {
         Mat  mat1 = Mat.eye(3,3, CV_8UC1);
         out.println("mat1 = ");
@@ -40,11 +44,39 @@ public class HelloCv {
         //convert to greyscale
         //Mat mat = imread("images/cobh.jpg", Imgcodecs.IMREAD_GRAYSCALE);
         Mat mat = imread("images/cobh.jpg");
-        Mat submat = mat.submat(100,100,100,100);
+        Mat submat = mat.submat(100,250,100,250);
+        Imgproc.blur(submat, submat, new Size(25.0, 25.0));
         out.println(submat);
         out.println(mat);
         out.println("mat = " + mat.width() + " x " + mat.height() + " , " + mat.type());
         MatOfInt moi = new MatOfInt(Imgcodecs.CV_IMWRITE_PNG_COMPRESSION, 9);
-        imwrite("images/output.png", submat);
+        imwrite("images/output.png", mat);
+
+        Mat rubik = new Mat(200,300,CV_8UC3);
+        setColors(mat, false, 1);
+        setColors(mat, true, 0);
+        Imgcodecs.imwrite("images/rubik.jpg", rubik);
+       
+    }
+
+    static void setColors(Mat mat, boolean comp, int row){
+        Scalar RED = new Scalar(0,0,255);
+        Scalar GREEN = new Scalar(0,255,0);
+        Scalar BLUE = new Scalar(255,0,0);
+        Scalar CYAN = new Scalar(255,255,0);
+        Scalar MAGENTA = new Scalar(255,0,255);
+        Scalar YELLOW = new Scalar(0,255,255);
+        for(int i=0; i<3; i++){
+            Mat sub = mat.submat(row*100, row*100+100, i*100, i*100+100);
+            if(comp){
+                if(i==0) sub.setTo(RED);
+                if(i==1) sub.setTo(GREEN);
+                if(i==2) sub.setTo(BLUE);
+            }else{
+                if(i==0) sub.setTo(CYAN);
+                if(i==1) sub.setTo(MAGENTA);
+                if(i==2) sub.setTo(YELLOW);
+            }
+        }
     }
 }
