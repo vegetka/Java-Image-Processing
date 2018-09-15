767978ae6e32c21ea9b6030434a375d6d83acf2d
vegetKa <vegetka@gmail.com>
2018-09-15 15:29:05 +0100

    Working

--- hellocv/java/HelloCv.java
+++ hellocv/java/HelloCv.java
@@ -1,11 +1,18 @@
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
+import static org.opencv.imgcodecs.Imgcodecs.imwrite;
 
 public class HelloCv {
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat  hello = Mat.eye(3,3, CvType.CV_8UC1);
+        Mat  hello = Mat.eye(150,150, CvType.CV_8UC1);
+        hello.setTo(new Scalar(180,80,250));
+        Mat sub = hello.submat(0,50,0,50);
+        sub.setTo(new Scalar(0,0,100));
+        imwrite("dev/hello.png", hello);
         System.out.println(hello.dump());
     }
 }
--- hellocv/project.clj
+++ hellocv/project.clj
@@ -6,8 +6,9 @@
   :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [opencv/opencv "3.3.1"]
-                 [opencv/opencv-native "3.3.1"]
+                 [opencv/opencv-native "3.3.1_4"]
                  ; [opencv/opencv-native "3.3.1" :classifier "osx"]
                  ; [opencv/opencv-native "3.3.1" :classifier "windows"]
                  ; [opencv/opencv-native "3.3.1" :classifier "linux"]
                  ])
+                 
